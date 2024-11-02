77406c847e6ca04a4054aa04524885aa9bc3b01a
Yohannes Taye <ytaye715@gmail.com>
2024-11-02 16:28:17 +0300

    change detection added

--- scripts/web_data_scrapping.py
+++ scripts/web_data_scrapping.py
@@ -5,7 +5,7 @@ import pandas as pd
 brent_data = yf.Ticker("BZ=F")  # "BZ=F" is the ticker symbol for Brent crude oil
 
 # Retrieve data from January 1, 1987, to December 31, 2023
-hist_data = brent_data.history(start="1987-01-01", end="2023-12-31")
+hist_data = brent_data.history(start="1987-01-01", end="2022-12-31")
 
 # Display the first few rows of the historical data
 print(hist_data.head())
